9b32c3322fb50bae74b20edd9e550a7428d99dc7
root <gjaendra.chn@gmail.com>
2020-12-07 15:35:28 +0530

    python file

---
+++ list.py
@@ -0,0 +1,15 @@
+import os
+list1=[8,9,3,6,10]
+print("the reversed list1 is",list1)
+
+list2=[5,3,27,1,2,11]
+list2.sort()
+print("sorted list2=",list2)
+
+list3=[]
+list3=list1.copy()
+print("list3=",list3)
+
+indexvalue=list2[2:6]
+print("indexvalues are",indexvalue)
+
